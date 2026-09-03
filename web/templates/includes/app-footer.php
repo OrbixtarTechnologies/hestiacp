706a50977703c9f136710313c734faa2c0c56727
Usman Liaqat <137144786+OrbixtarTechnologies@users.noreply.github.com>
2026-09-03 07:29:28 -0700

    feat(ui): add control panel status footer

--- web/templates/includes/app-footer.php
+++ web/templates/includes/app-footer.php
@@ -1,10 +1,11 @@
 <footer class="app-footer">
-	<div class="container">
+	<div class="container app-footer-inner">
 		<p>
 			<a href="https://hestiacp.com/" class="app-footer-link" target="_blank">
 				Hestia Control Panel
 			</a>
 			v<?= $_SESSION["VERSION"] ?>
 		</p>
+		<p class="app-footer-status"><i class="fas fa-circle-check"></i> <?= _("Control panel online") ?></p>
 	</div>
 </footer>
